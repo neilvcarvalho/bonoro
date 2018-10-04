cb87076c1b31ea9b4d919d0c66203039074d63e8
Neil Carvalho <me@neil.pro>
2018-10-04 20:02:48 -0300

    Move o app para o Github

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# bonoro
-Gerador de nomes do Bolovo
+# Bonoro
+Gerador de nomes do Bolovo. Qualquer semelhança com coisas da vida real é mera coincidência.
+
+# Restrições
+Todos os nomes do Bulbasauro devem começar com `B` e terminar com `o`. Isso impede belos nomes como Salnorabo, mas gera nomes mais consistentes.
+
---
+++ js/app.js
@@ -0,0 +1,59 @@
+var dados = {
+  "prefixos": [
+    "Bolso",
+    "Bono",
+    "Boro",
+    "Bulba",
+    "Bozo",
+    "Bonso",
+    "Bo",
+    "Barto",
+    "Bonzi",
+    "Baro",
+    "Bunda",
+    "Boto",
+    "Bolo",
+    "Bonos",
+    "Buce",
+    "Bucen",
+    "Bosni",
+  ],
+  "sufixos": [
+    "naro",
+    "sauro",
+    "ro",
+    "tário",
+    "mario",
+    "çaro",
+    "maro",
+    "saro",
+    "xaro",
+    "nauro",
+    "valdo",
+    "vo",
+    "tauro",
+    "nazo",
+  ]
+}
+var prefixos = dados["prefixos"];
+var sufixos = dados["sufixos"];
+
+function novoNomeBonoro() {
+  var prefixo = prefixos[Math.floor(Math.random() * prefixos.length)];
+  var sufixo = sufixos[Math.floor(Math.random() * sufixos.length)];
+
+  var nome = prefixo + sufixo;
+
+  // Não quero dar nomes de pessoas reais, seria coincidência demais
+  if (nome !== "Bolsonaro") {
+    return nome;
+  } else {
+    return novoNomeBonoro();
+  }
+}
+
+function imprimeNovoNome() {
+  document.getElementById("bonoro").innerHTML = novoNomeBonoro();
+}
+
+imprimeNovoNome();
